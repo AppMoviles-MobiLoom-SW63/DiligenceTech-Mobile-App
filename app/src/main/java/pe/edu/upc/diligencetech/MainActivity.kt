c707d4db034ce2defd6e25b8701c46d0de448e8b
diegooo01 <129528729+diegooo01@users.noreply.github.com>
2024-09-21 00:55:35 -0500

    Inital commit

---
+++ app/src/main/java/pe/edu/upc/diligencetech/MainActivity.kt
@@ -0,0 +1,20 @@
+package pe.edu.upc.diligencetech
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import pe.edu.upc.diligencetech.ui.theme.DiligenceTechAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiligenceTechAppTheme {
+
+            }
+        }
+    }
+}
+
