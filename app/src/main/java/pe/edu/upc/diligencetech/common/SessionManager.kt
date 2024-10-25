e7b21b8e54064cf2bb74f599b132d65eb6da58a5
hernancit0 <ernanemiliom12@gmail.com>
2024-10-24 19:09:27 -0500

    feat: session manager

---
+++ app/src/main/java/pe/edu/upc/diligencetech/common/SessionManager.kt
@@ -0,0 +1,9 @@
+package pe.edu.upc.diligencetech.common
+
+import javax.inject.Inject
+
+class SessionManager @Inject constructor() {
+    fun logout() {
+        // Lógica de cierre de sesión
+    }
+}
